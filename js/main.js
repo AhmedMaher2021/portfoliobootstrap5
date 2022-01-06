844c234960ea3ef4b5c97751f2df0a6f69124b63
AhmedMaher2021 <78690715+AhmedMaher2021@users.noreply.github.com>
2022-01-06 16:25:57 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWhtZWRtYWhlcmFiZGVsaGFsaW1haG1lZCIsImEiOiJja3IzaTkyYnIybHpoMnZxYWoycW84Z2Q3In0.IAQr-Mgj0Qk7Y1xkjMKA6w';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+zoom: 1,
+});
